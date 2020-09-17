a9e8746982882c9e4ec243cd136276f6d65dd588
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-09-17 19:42:44 +0630

    Update UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Counter from './Counter';
 import './App.css';
-
 function App() {
-  const [counter, setCounter] = useState(0);
   return (
-    <div className="counter">
-      <h1 className="counter__label">
-        {counter}
-      </h1>
-      <button className="counter__increaseBtn" onClick={() => setCounter(counter + 1)}>Increase</button>
-      <button className="counter__decreaseBtn" onClick={() => setCounter(counter - 1)}>Decrease</button>
+    <div className="App">
+      <Counter />
     </div>
   );
 }
---
+++ src/Counter.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
+import './Counter.css';
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+    const [error, setError] = useState('');
+    const onIncreaseClick = () => {
+        if(counter <= 9){
+            setCounter(counter + 1);
+            setError('');
+        } else{
+            setError('Cannot increase greater than 10!')
+        }
+    }
+    const onDecreaseClick = () => {
+        if(counter >= 1){
+            setCounter(counter - 1);
+            setError('');
+        } else{
+            setError('Cannot decrease less than or equal 0!')
+        }
+    }
+    const onResetClick = () => {
+        setCounter(0);
+        setError('');
+    }
+    return (
+        <Paper elevation={3} className={'counter'}>
+            <h1>Counter App</h1>
+            <h3 className="counter__label">
+                {counter}
+            </h3>
+            <div className="counter__buttons">
+                <Button variant="contained" onClick={onResetClick}>
+                    Reset
+                </Button>
+                <Button variant="contained" color="secondary" className="counter__decreaseBtn" onClick={onDecreaseClick}>
+                    Decrease
+                </Button>
+                <Button variant="contained" color="primary" className="counter__increaseBtn" onClick={onIncreaseClick}>
+                    Increase
+                </Button>
+            </div>
+            {
+                error.length > 0 && <Alert severity="error">{error}</Alert>
+            }
+        </Paper>
+    )
+}
+export default Counter;
